bdc35af5954b84dea2206e3dd4f4604416c9afaa
Voskan Voskanyan <voskanvoskanyan@yandex.com>
2024-03-29 18:10:55 +0400

    update docker settings and adding k8s

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,10 +4,12 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+const port = process.env.PORT || 3000;
+
 app.get("/api/users/currentuser", (req: Request, res: Response) => {
   res.send("Hi there!");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Listening on port 3000");
 });
