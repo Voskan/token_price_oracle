970328ba7128a6c850e9a46da8304848298ef6d3
Voskan Voskanyan <voskanvoskanyan@yandex.com>
2024-03-30 08:33:01 +0400

    adding skaffold and nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -11,5 +11,5 @@ app.get("/api/users/currentuser", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log("Listening on port 3000");
+  console.log("Listening on port 3000 !!!!!!");
 });
